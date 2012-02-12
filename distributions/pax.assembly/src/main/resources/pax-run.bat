5cd5506c12de22029da2812be9b103812f1bee1a
Mirko Jahn <mirkojahn@gmail.com>
2012-02-13 00:09:44 +0100

    configured logging, removed obsolete dependencies and isolated test modules in different profile

--- distributions/pax.assembly/src/main/resources/pax-run.bat
+++ distributions/pax.assembly/src/main/resources/pax-run.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 cd pax
-echo Welcome to ${application.name}
+echo Welcome to ${application.name} 
 
 if "%1%"=="test" goto test
 if "%1%"=="debug" goto mem
@@ -10,16 +10,16 @@ if "%1%"=="debug" goto mem
 goto default
 
 :test
-echo starting with web server provisioning (not useful right now - no Servlet is implemented just yet)
-CALL java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax\pax-runner.jar" org.ops4j.pax.runner.Run --args="file:runner-grizzly.args" %2 %3 %4 %5 %6 %7 %8 %9
+echo starting now with demo components.
+CALL java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax\pax-runner.jar" org.ops4j.pax.runner.Run --args="file:runner-test.args" %2 %3 %4 %5 %6 %7 %8 %9
 goto:eof
 
 :debug
-echo starting with remote debugging enabled
+echo starting now with remote debugging enabled.
 CALL java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax\pax-runner.jar" org.ops4j.pax.runner.Run --args="file:runner-debug.args" %2 %3 %4 %5 %6 %7 %8 %9
 goto:eof
 
 :default
-echo starting ${application.name}
+echo starting now ${application.name}
 CALL java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax\pax-runner.jar" org.ops4j.pax.runner.Run %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto:eof
--- distributions/pax.assembly/src/main/resources/pax-run.sh
+++ distributions/pax.assembly/src/main/resources/pax-run.sh
@@ -15,8 +15,8 @@ fi
 if [ "$1" ]
 	then
 	  case $1 in
-        test)	printf "Starting with web server provisioning (not useful right now - no Servlet is implemented just yet)\n"
-                cd pax; java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/pax-runner.jar org.ops4j.pax.runner.Run --args="file:runner-grizzly.args" $2 $3 $4 $5 $6 $7 $8 $9
+        test)	printf "Starting with demo components. \n"
+                cd pax; java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/pax-runner.jar org.ops4j.pax.runner.Run --args="file:runner-test.args" $2 $3 $4 $5 $6 $7 $8 $9
                 ;;
         debug)	printf "Starting with debug options set. \n"
                 cd pax; java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/pax-runner.jar org.ops4j.pax.runner.Run --args="file:runner-debug.args" $2 $3 $4 $5 $6 $7 $8 $9
