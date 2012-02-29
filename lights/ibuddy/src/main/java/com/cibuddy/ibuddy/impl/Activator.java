1689e64ca98aa0cfd054c9ab75efe4f00d37fc6f
Mirko Jahn <mirkojahn@gmail.com>
2012-02-29 02:56:02 +0100

    Improved logging and handling of none linux devices (linux is bad for identifying the correct HID device for a working i-Buddy - currently 2 instead of 1)

--- lights/ibuddy/src/main/java/com/cibuddy/ibuddy/impl/Activator.java
+++ lights/ibuddy/src/main/java/com/cibuddy/ibuddy/impl/Activator.java
@@ -6,27 +6,37 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.util.tracker.ServiceTracker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author mirkojahn
  */
-public class Activator implements BundleActivator{
+public class Activator implements BundleActivator {
 
-    //private static String filterString = "(&(" + Constants.OBJECTCLASS + "="+HIDDeviceInfo.class.getName()+")("+HIDServiceConstants.VENDOR_ID+"=4400)("+HIDServiceConstants.USAGE+">=1))";
-    // on linux, we do not know which of the two exposed hid devices is actually the correct one. We have to try both at runtime unfortunately.
-    private static String filterString = "(&(" + Constants.OBJECTCLASS + "="+HIDDeviceInfo.class.getName()+")("+HIDServiceConstants.VENDOR_ID+"=4400))";
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
     
+    private static String filterString = "(&(" + Constants.OBJECTCLASS + "=" + HIDDeviceInfo.class.getName() + ")(" + HIDServiceConstants.VENDOR_ID + "=4400)(" + HIDServiceConstants.USAGE + ">=1))";
+    // on linux, we do not know which of the two exposed hid devices is actually the correct one. We have to try both at runtime unfortunately.
+    private static String filterString_linux = "(&(" + Constants.OBJECTCLASS + "=" + HIDDeviceInfo.class.getName() + ")(" + HIDServiceConstants.VENDOR_ID + "=4400))";
     private static BundleContext ctx;
     private static ServiceTracker lightsTracker = null;
     private HIDDeviceInfoServiceTrackerCustomizer customizer;
-    
-    
+
     @Override
     public void start(BundleContext bc) throws Exception {
         ctx = bc;
         customizer = new HIDDeviceInfoServiceTrackerCustomizer();
-        // some important services we might need
+        
+        // in case of unix, we can't make enough assumptions. Be less restrictive
+        if(isUnix()) {
+            // ultimately this will result in 2 hid devices, where only one will work
+            filterString = filterString_linux;
+            LOG.info("Using relaxed filter string for linux. This will result "
+                    + "in 2 i-Buddy devices, although only one does in fact work! "
+                    + "Test which one before applying your configuration!");
+        }
         lightsTracker = new ServiceTracker(bc, bc.createFilter(filterString), customizer);
         lightsTracker.open();
 
@@ -34,19 +44,25 @@ public class Activator implements BundleActivator{
 
     @Override
     public void stop(BundleContext bc) throws Exception {
-        if(customizer != null){
+        if (customizer != null) {
             customizer.close();
             customizer = null;
         }
-        
-        if(lightsTracker != null){
+
+        if (lightsTracker != null) {
             lightsTracker.close();
             lightsTracker = null;
         }
     }
-    
-    public static BundleContext getBundleContext(){
+
+    public final static BundleContext getBundleContext() {
         return ctx;
     }
-    
+
+    public final static boolean isUnix() {
+        String os = System.getProperty("os.name").toLowerCase();
+        // linux or unix
+        return (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0);
+
+    }
 }
