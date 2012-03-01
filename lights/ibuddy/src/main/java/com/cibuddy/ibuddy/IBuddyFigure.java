fdf30c6c9b6bd12412a87330d39f77fac2e15218
Mirko Jahn <mirkojahn@gmail.com>
2012-03-01 02:12:27 +0100

    Introduced new setup command

--- lights/ibuddy/src/main/java/com/cibuddy/ibuddy/IBuddyFigure.java
+++ lights/ibuddy/src/main/java/com/cibuddy/ibuddy/IBuddyFigure.java
@@ -15,6 +15,10 @@ public interface IBuddyFigure {
         (byte) 0x55, (byte) 0x53, (byte) 0x42, (byte) 0x43, 
         (byte) 0x00, (byte) 0x40, (byte) 0x02, (byte) 0x00};
     
+    byte [] SETUP_COMMAND = new byte[] { (byte) 0x00,
+        (byte) 0x22, (byte) 0x09, (byte) 0x00, (byte) 0x02, 
+        (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+    
     void close() throws IOException;
 
     void open() throws IOException;
