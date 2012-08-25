1780f62c8a6155da04c1cd85e5734cb4c09ec53c
Mirko Jahn <mirkojahn@gmail.com>
2012-08-25 19:20:57 +0200

    Removed obsolete pax based assembly.

--- distributions/pax.assembly/src/main/resources/pax-rund.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SETLOCAL
-set _SCRIPTS_=%~dp0
-
-echo Welcome to ${application.name}
-echo The deamon script currently does not support all features of the plain cmd script. You might have to tweak it first.
-java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax-runner.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %*
