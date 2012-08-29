3338ee32ca9780af3db90e1ecbb20e60fa41ac3d
Mirko Jahn <mirkojahn@gmail.com>
2012-08-29 21:54:50 +0200

    Updated package documentation to attribute actual authors and hint at system prerequisite.

--- drivers/hid/src/main/java/com/codeminders/hidapi/package-info.java
+++ drivers/hid/src/main/java/com/codeminders/hidapi/package-info.java
@@ -1,13 +1,30 @@
 /**
- * This package is copied (and slightly modified) from the javahidapi project.
+ * This package is copied (and slightly modified) from the <a title="javahidapi homepage" src="http://code.google.com/p/javahidapi/">javahidapi project</a>.
  * 
+ * <p>
  * The main differences between this and the javahidapi project are that not all
  * classes are present here and the code was enhanced on a few occasions, as well 
  * as the javadoc, to better support users of the library.
+ * </p>
  * 
+ * <p>
  * To interact in OSGi with USB devices, look for <code>com.codeminders.hidapi.HIDDeviceInfo</code>
  * class based services. This library will expose all identified USB devices as
  * such services, so you should not worry about how to obtain those.
+ * </p>
+ * 
+ * <b>Attention!</b>
+ * <p>
+ * In order to use the native code you have to ensure the proper environment is
+ * available on you operating system. In case of Windows and Mac OS X, you should
+ * be good to go. However on Linux libusb and others in a specific version are
+ * required. Check these links to ensure everything works as expected:
+ * <ul>
+ *  <li><a title="javahidapi homepage" src="http://code.google.com/p/javahidapi/">javahidapi project</a></li>
+ *  <li><a title="libusb homepage" src="http://www.libusb.org/"></a></li>
+ *  <li><a title="libusb github page" src="https://github.com/signal11/hidapi"></a></li>
+ * </ul>
+ * </p>
  * @version 1.1
  */
 package com.codeminders.hidapi;
