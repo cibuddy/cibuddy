4df3764dcaefd4ee3e9f9870b56e60e047169b09
Mirko Jahn <mirkojahn@gmail.com>
2012-09-30 21:40:32 +0200

    clean-up

--- lights/ibuddy/src/main/java/com/cibuddy/ibuddy/IBuddyDevil.java
+++ lights/ibuddy/src/main/java/com/cibuddy/ibuddy/IBuddyDevil.java
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cibuddy.ibuddy;
 
 import com.codeminders.hidapi.HIDDeviceInfo;
