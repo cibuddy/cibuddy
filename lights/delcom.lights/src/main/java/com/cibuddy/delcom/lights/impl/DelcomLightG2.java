1b404f33decb74cceecbc829fb11802d7a4ac2cd
Mirko Jahn <mirkojahn@gmail.com>
2012-10-11 01:51:54 +0200

    Changed the way commands are written to the  delcom light. This fixes issue #17 on my machine, without failing on other OS's. closed #17

--- lights/delcom.lights/src/main/java/com/cibuddy/delcom/lights/impl/DelcomLightG2.java
+++ lights/delcom.lights/src/main/java/com/cibuddy/delcom/lights/impl/DelcomLightG2.java
@@ -57,8 +57,8 @@ public class DelcomLightG2 implements IDelcomLight{
         }
         byte[] buffer = SET_STRUCTURE.clone();
         buffer[SET_BYTE] = color.getCode();
-//        device.sendFeatureReport(buffer);
-        device.write(buffer);
+        device.sendFeatureReport(buffer);
+//        device.write(buffer);
         currentColor = color;
     }
 
